78dcb452a3251c018fd79dacf427c3b7b99d431a
engpetermwangi <pmwangi155@gmail.com>
2023-01-08 22:00:32 +0300

    setting error log level, commonjs imports for type

--- firestore.rules.test.js
+++ firestore.rules.test.js
@@ -1,14 +1,14 @@
-import {
-  initializeTestEnvironment,
-  assertSucceeds,
-  assertFails,
-} from "@firebase/rules-unit-testing";
-import { getDoc, doc, setDoc } from "@firebase/firestore";
+const testing = require("@firebase/rules-unit-testing");
+const { initializeTestEnvironment, assertSucceeds, assertFails } = testing;
+const { getDoc, doc, setDoc, setLogLevel } = require("@firebase/firestore");
 
 const PROJECT_ID = "YOUR_UNIQUE_PROJECT_ID";
 const FIRESTORE_EMULATOR_HOST = "localhost";
 const FIRESTORE_EMULATOR_PORT = 8080;
 
+/**
+ * @type testing.RulesTestEnvironment
+ */
 let env;
 
 beforeAll(async () => {
@@ -16,6 +16,7 @@ beforeAll(async () => {
     projectId: PROJECT_ID,
     firestore: { host: FIRESTORE_EMULATOR_HOST, port: FIRESTORE_EMULATOR_PORT },
   });
+  setLogLevel("error");
 });
 
 afterEach(async () => {
@@ -33,11 +34,12 @@ test("public user profile can be read by anyone, authenticated or not", async ()
 
 test("public user profile can only be updated by authenticated user updating own profile", async () => {
   const USER_ID = "user123";
+  const ANOTHER_USER_ID = "user12345";
   const db = env.authenticatedContext(USER_ID).firestore();
   await assertSucceeds(
     setDoc(doc(db, `users/${USER_ID}`), { name: "New Name" })
   );
   await assertFails(
-    setDoc(doc(db, `users/anotherUserId`), { name: "Yet Another Name" })
+    setDoc(doc(db, `users/${ANOTHER_USER_ID}`), { name: "Yet Another Name" })
   );
 });
